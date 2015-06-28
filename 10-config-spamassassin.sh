0c9b468e6e0425c4f48203689c280f0a18383f4a
Loh Siu Yin <siuyin@beyondbroadcast.com>
2015-06-29 00:53:58 +0800

    Limit amavis server to 1.

--- 10-config-spamassassin.sh
+++ 10-config-spamassassin.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 echo "trusted_networks $MAILSVR_IP" >> /etc/spamassassin/local.cf 
 sed -i -e's/--max-children\s\+[0-9]\+/--max-children 1/' /etc/default/spamassassin
+sed -i -e'/Do not modify anything below this line/i$max_servers = 1;' /etc/amavis/conf.d/50-user
+
