850b2c25ec970060ca7229963a38c220d3e8d74b
Loh Siu Yin <siuyin@beyondbroadcast.com>
2015-06-29 01:06:38 +0800

    modified:   amavis/amavismon.sh

--- amavis/amavismon.sh
+++ amavis/amavismon.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 /etc/init.d/amavis start
-ps ax|grep -v grep|grep 'amavisd-new (master)'
+ps ax|grep -v grep|grep 'amavisd-new'
 RC=$?
 while [ $RC -eq 0 ]; do
   sleep 600 
-  ps ax|grep -v grep|grep 'amavisd-new (master)'
+  ps ax|grep -v grep|grep 'amavisd-new'
   RC=$?
 done
